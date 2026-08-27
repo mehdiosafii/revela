c976dfe3f56ab3cdeced28921cae88176d191068
mehdiosafii <ihsanielmehdi@gmail.com>
2026-08-27 21:14:27 +0100

    Allow POST for admin queries

--- server/app.ts
+++ server/app.ts
@@ -16,6 +16,7 @@ app.use("/api/trpc/*", async (c) => {
     req: c.req.raw,
     router: appRouter,
     createContext,
+    allowMethodOverride: true,
   });
 });
 app.all("/api/*", (c) => c.json({ error: "Not Found" }, 404));
